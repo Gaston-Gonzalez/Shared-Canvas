f07046210b61be44c91dab4889d9561eb5d19718
Gaston-Gonzalez <ggonzalez@effectussoftware.com>
2022-06-16 17:13:00 -0300

    Name input

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import styled from "styled-components";
+
+import "./App.css";
+import Canvas from "./canvas/Canvas";
+import { TextField } from "@mui/material";
+
+const BodyContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 4rem auto;
+`;
+
+const CssTextField = styled(TextField)({
+  color: "#FFFFFF",
+  "& label.Mui-focused": {
+    color: "white",
+  },
+  "& .MuiInput-underline:after": {
+    borderBottomColor: "white",
+  },
+  "& .MuiOutlinedInput-root": {
+    "& fieldset": {
+      borderColor: "white",
+    },
+    "&:hover fieldset": {
+      borderColor: "white",
+    },
+    "&.Mui-focused fieldset": {
+      borderColor: "white",
+    },
+  },
+});
 
 function App() {
+  const [name, setName] = useState("");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">Shared Canvas</header>
+      <BodyContainer>
+        <CssTextField
+          id="name-input"
+          InputProps={{
+            style: {
+              borderColor: "#FFFFFF",
+            },
+          }}
+          InputLabelProps={{
+            style: {
+              color: "#FFFFFF",
+            },
+          }}
+          inputProps={{
+            style: {
+              color: "#FFFFFF",
+            },
+          }}
+          variant="outlined"
+          label="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        {name}
+        <Canvas name={name} />
+      </BodyContainer>
     </div>
   );
 }
---
+++ src/canvas/Canvas.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Canvas = ({ name }) => {
+  return <div>{name}</div>;
+};
+
+export default Canvas;
